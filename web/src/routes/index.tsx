5e9e2089f832f5754073799c2b907e3c6672001c
jowpurcinelli <jaopedropurcinelli@gmail.com>
2020-09-12 19:12:04 -0300

    routes

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -11,17 +11,18 @@ import Profile from '../pages/Profile';
 
 import Dashboard from '../pages/Dashboard';
 
-const Routes: React.FC = () => (
-  <Switch>
-    <Route path="/" exact component={SignIn} />
-    <Route path="/signup" component={SignUp} />
+const Routes: React.FC = ( ) => {
+  return(
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+      <Route path="/signup" component={SignUp} />
+      <Route path="/forgot-password" component={ForgotPassword} />
+      <Route path="/reset-password" component={ResetPassword} />
 
-    <Route path="/dashboard" component={Dashboard} isPrivate />
-
-    <Route path="/forgot" component={ForgotPassword} />
-    <Route path="/reset-password" component={ResetPassword} />
-    <Route path="/profile" component={Profile} isPrivate />
-  </Switch>
+      <Route path="/profile" component={Profile} isPrivate />
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+    </Switch>
 );
+};
 
 export default Routes;
