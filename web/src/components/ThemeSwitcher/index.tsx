2803ff602f3dd1f929bed3275175417fd03e2db0
jowpurcinelli <jaopedropurcinelli@gmail.com>
2020-09-12 17:11:29 -0300

    themeSwitcherComponent

--- web/src/components/ThemeSwitcher/index.tsx
+++ web/src/components/ThemeSwitcher/index.tsx
@@ -2,3 +2,33 @@ import React, { useContext } from 'react';
 import Switch from 'react-switch';
 import { ThemeContext } from 'styled-components';
 
+import { Container } from './styles';
+
+interface Props {
+    toggleTheme( ): void
+}
+
+const ThemeSwitcher : React.FC<Props> = ({ toggleTheme }) => {
+    const { colors, title } = useContext(ThemeContext)
+
+    return (
+        <Container>
+            <Switch
+                onchange = { toggleTheme }
+                checkedIcon = { false }
+                uncheckedIcon = { false }
+                checked = { title === 'dark' }
+                width = { 40 }
+                height = { 10 }
+                handleDiameter = { 20 }
+                offColor = { colors.secundary }
+                onColor = { colors.text }
+                offHandleColor = { colors.secundary }
+                onHandleColor = { colors.text }
+
+            />
+        </Container>
+    )
+}
+
+export default ThemeSwitcher;
---
+++ web/src/components/ThemeSwitcher/styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Container = styled.div `
+    position : absolute;
+    margin: 7px;
+`
