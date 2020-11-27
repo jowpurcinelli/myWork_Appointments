1c6ffa9cfbda90ff05dd3005e423044761cb70ef
João Pedro Purcinelli <jppurcinelli@gmail.com>
2020-11-27 15:06:09 -0300

    Create connection.ts

---
+++ backend/connection.ts
@@ -0,0 +1,16 @@
+import React from 'react';
+import express from 'express';
+import {user, appointment, id} from '';
+
+
+const connect = {express} 
+const specificApp = 
+
+const showAppointments () => {
+  connect.get('/appointments')
+}
+
+
+const postAppointments () => {
+  connect.post($)
+}
